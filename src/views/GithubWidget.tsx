eafd3d46f9a9cd1b73206b48959c15b393f9636c
VovaTereschenko <t.rimidalv@gmail.com>
2022-11-15 00:36:56 +0300

    Use DebouncedTextField

--- src/views/GithubWidget.tsx
+++ src/views/GithubWidget.tsx
@@ -1,14 +1,11 @@
+import * as React from "react";
 import { useGithubRepositoriesRequest } from "../hooks/useGithubRepositoriesRequest";
-
+import { DebouncedTextField } from "../components/DebouncedTextField";
 
 const GithubWidget = () => {
-  const searchValue = 'mui'
-  const resultsCount = 10
-  const {
-    loading,
-    error,
-    repositoryMap,
-  } = useGithubRepositoriesRequest({
+  const resultsCount = 10;
+  const [searchValue, setSearchValue] = React.useState("mui");
+  const { loading, error, repositoryMap } = useGithubRepositoriesRequest({
     searchValue,
     resultsCount,
   });
@@ -16,8 +13,16 @@ const GithubWidget = () => {
   console.log("repositoryMap", repositoryMap);
 
   return (
-    <div>Hello Github Widget</div>
-  )
-}
+    <div>
+      Hello Github Widget
+      <DebouncedTextField
+        defaultValue={"mui"}
+        onChange={(val: string) => {
+          setSearchValue(val);
+        }}
+      />
+    </div>
+  );
+};
 
 export { GithubWidget };
